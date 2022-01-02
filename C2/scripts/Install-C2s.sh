ec8a6db7651159e2530fe96ac09ab92ec47fef41
rev10d <31927659+rev10d@users.noreply.github.com>
2022-01-02 12:41:23 -0700

    Something broke in the construct Dave.

--- C2/scripts/Install-C2s.sh
+++ C2/scripts/Install-C2s.sh
@@ -5,17 +5,20 @@
 # Includes some apt installs, some git clones and pip
 # Script available for wide distribution
 # Run as root, please, it's just easier that way
-
+sudo -s
 
 # housekeeping
 apt update
 apt upgrade -y
 
-# I use virtual environments to containerize python-based tooling
+
+# Use virtual environments to containerize python-based tooling
 apt install python3-pip python3-venv zip -y
 
-# Add nmap 
+
+# Add nmap whois
 apt install nmap whois -y
+
 
 # I use /opt/ to house tools
 cd /opt/
