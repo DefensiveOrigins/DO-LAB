55d9955da3517e8a2f98d1ae5756957aba297f62
Relkci <kickler@blackhillsinfosec.com>
2023-12-11 11:41:56 -0700

    move to new tool installer

--- C2/scripts/Install-Tools.sh
+++ C2/scripts/Install-Tools.sh
@@ -1,193 +1,179 @@
+## Script is currently choking on a kernel mismatch and pending reboot
+
 #!/bin/bash
 
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
 
-# Run as root, please, it's just easier that way
-# this runs in a crontab, so don't run as sudo.  If you ever need to run manually, call sudo first
-# sudo -s
+# apt packages
+# apt-get update -y && apt-get full-upgrade -y
+apt-get update -y
+apt-get install python3 -y
+apt-get install virtualenv -y
+apt-get install python3-distutils python3-virtualenv libssl-dev libffi-dev python-dev-is-python3 build-essential smbclient libpcap-dev -y
+apt-get install vim-nox htop ncat rlwrap golang jq feroxbuster silversearcher-ag testssl.sh crackmapexec nmap masscan proxychains4 -y
+apt-get install python3.11-venv -y
+apt-get install onesixtyone snmp-mibs-downloader -y
+apt-get install zsh
+# Install latest metasploit
+gem install bundler
+apt-get install metasploit-framework -y
 
-echo "Time: $(date). Starting Installing Tools" >> /etc/DOAZLAB/DOAZLABLog
+# remove outdated packages
+apt-get autoremove -y
 
-cd /etc/DOAZLAB
-mkdir /etc/DOAZLAB/install-logs/
+# update snmp.conf
+sed -e '/mibs/ s/^#*/#/' -i /etc/snmp/snmp.conf
 
+# Repos
+[[ ! -d /opt/EyeWitness ]] && git clone https://github.com/RedSiege/EyeWitness.git /opt/EyeWitness
+[[ ! -d /opt/testssl.sh ]] && git clone --depth 1 https://github.com/drwetter/testssl.sh.git /opt/testssl.sh
+[[ ! -d /opt/Responder ]] && git clone https://github.com/lgandx/Responder.git /opt/Responder
+[[ ! -d /opt/impacket ]] && git clone https://github.com/fortra/impacket.git /opt/impacket
+[[ ! -d /opt/BloodHound.py ]] && git clone https://github.com/fox-it/BloodHound.py.git /opt/BloodHound.py
+[[ ! -d /opt/Certipy ]] && git clone https://github.com/ly4k/Certipy.git /opt/Certipy
+[[ ! -d /opt/Coercer ]] && git clone https://github.com/p0dalirius/Coercer.git /opt/Coercer
+[[ ! -d /opt/mitm6 ]] && git clone https://github.com/dirkjanm/mitm6.git /opt/mitm6
+[[ ! -d /opt/PCredz ]] && git clone https://github.com/lgandx/PCredz.git /opt/PCredz
+[[ ! -d /opt/certsync ]] && git clone https://github.com/zblurx/certsync.git /opt/certsync
+[[ ! -d /opt/pyLAPS ]] && git clone https://github.com/p0dalirius/pyLAPS.git /opt/pyLAPS
+[[ ! -d /opt/CrackMapExec ]] && git clone https://github.com/byt3bl33d3r/CrackMapExec.git /opt/CrackMapExec
 
-# Install python3.10
-echo "Time: $(date). -- 10 --  python3.10" >> /etc/DOAZLAB/DOAZLABLog
+cat << 'EOF' >> "${HOME}/.screenrc"
+termcapinfo * ti@:te@
+caption always
+caption string "%{kw}%-w%{wr}%n %t%{-}%+w"
+startup_message off
+defscrollback 1000000
+EOF
 
-apt install python3.10 -y | tee -a /etc/DOAZLAB/install-logs/python3.10.log
-apt update |tee -a /etc/DOAZLAB/install-logs/apt-update-after-python.log
+# setup GOPATH. Lots of confusion about GOPATH and GOMODULES
+# this will need rectified for bash or the implant / nux build needs shell swapped to zsh
+# see https://zchee.github.io/golang-wiki/GOPATH/ and https://maelvls.dev/go111module-everywhere/ for more info
+# TL:DR
+# GOPATH is still supported even though it has been replaced by Go modules and is technically deprecated since Go 1.16, BUT, you can still use GOPATH to specify where you want your go binaries installed.
+wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
+tar -C ~/ -xzf go1.21.4.linux-amd64.tar.gz
 
+[[ ! -d "${HOME}/go" ]] && mkdir "${HOME}/go"
+if [[ -z "${GOPATH}" ]]; then
+cat << 'EOF' >> "${HOME}/.zshrc"
 
-# Use virtual environments to containerize python-based tooling
-echo "Time: $(date). -- 20 --  Python venv" >> /etc/DOAZLAB/DOAZLABLog
-apt install python3.10-dev python3.10-venv -y | tee -a /etc/DOAZLAB/install-logs/020-python-devs.log
+# Add ~/go/bin to path
+[[ ":$PATH:" != *":${HOME}/go/bin:"* ]] && export PATH="${PATH}:${HOME}/go/bin"
+# Set GOPATH
+if [[ -z "${GOPATH}" ]]; then export GOPATH="${HOME}/go"; fi
+EOF
+fi
 
+[[ ":$PATH:" != *":${HOME}/go/bin:"* ]] && export PATH="${PATH}:${HOME}/go/bin"
+# Set GOPATH
+if [[ -z "${GOPATH}" ]]; then export GOPATH="${HOME}/go"; fi
 
-# pip installer for 3.9
-echo "Time: $(date). -- 30 --  Python pip" >> /etc/DOAZLAB/DOAZLABLog
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.10 get-pip.py | tee -a /etc/DOAZLAB/install-logs/030-pip.log
+# Install your favorite Go binaries
+# GO111MODULE=on go install github.com/mr-pmillz/gorecon/v2@latest Use the private version from our Gitlab
+GO111MODULE=on go install github.com/ropnop/kerbrute@latest
+GO111MODULE=on go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+GO111MODULE=on go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+GO111MODULE=on go install -v github.com/OJ/gobuster/v3@latest
+GO111MODULE=on go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+[[ -f "${HOME}/go/bin/nuclei" ]] && nuclei -ut || echo "nuclei not in ${HOME}/go/bin/"
 
+# create virtualenv dir.
+[[ ! -d "${HOME}/pyenv" ]] && mkdir "${HOME}/pyenv"
 
-# install a pre-req for a cffi package req
-# broke here, everything after is sus
-echo "Time: $(date). -- 40 --  libffi-dev" >> /etc/DOAZLAB/DOAZLABLog
-apt install libffi-dev -y | tee -a /etc/DOAZLAB/install-logs/040-libffi.log
+# ignore shellcheck warnings for source commands
+# shellcheck source=/dev/null
+install_with_virtualenv() {
+    REPO_NAME="$1"
+    PYENV="${HOME}/pyenv"
+    if [ -d "/opt/${REPO_NAME}" ]; then
+        cd "/opt/${REPO_NAME}" || exit 1
+        virtualenv -p python3 "${PYENV}/${REPO_NAME}"
+        . "${PYENV}/${REPO_NAME}/bin/activate"
+        python3 -m pip install -U wheel setuptools
+        # first, ensure that requirements.txt deps are installed.
+        [[ -f requirements.txt ]] && python3 -m pip install -r requirements.txt
+        # python3 setup.py install is deprecated in versions >= python3.9.X
+        # python3 -m pip install . will handle the setup.py file for you.
+        [[ -f setup.py || -f pyproject.toml ]] && python3 -m pip install .
+        deactivate
+        cd - &>/dev/null || exit 1
+    else
+        echo -e "${REPO_NAME} does not exist."
+    fi
+}
 
+install_with_virtualenv Responder
+install_with_virtualenv impacket
+install_with_virtualenv BloodHound.py
+install_with_virtualenv Certipy
+install_with_virtualenv Coercer
+install_with_virtualenv mitm6
 
-# Add nmap whois zip
-echo "Time: $(date). -- 50 --  nmap net-tools whois zip" >> /etc/DOAZLAB/DOAZLABLog
-apt install nmap net-tools whois zip -y | tee -a /etc/DOAZLAB/install-logs/050-apt.log
+install_pipx() {
+    # check if pipx is already installed
+    PIPX_EXISTS=$(which pipx)
+    if [ -z "$PIPX_EXISTS" ]; then
+        # Get the Python 3 version
+        python_version_output=$(python3 --version 2>&1)
+        python_version=$(echo "$python_version_output" | awk '{print $2}' | cut -d '.' -f 1,2)
 
+        if [ "$python_version" == "3.10" ] || [ "$python_version" == "3.11" ] || [ "$python_version" == "3.12" ]; then
+            python3 -m pip install pipx --break-system-packages || python3 -m pip install pipx
+        else
+            python3 -m pip install pipx || python3 -m pip install pipx --user
+        fi
+    fi
+}
 
-# First up: impacket
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-echo "Time: $(date). -- 60 --  impacket" >> /etc/DOAZLAB/DOAZLABLog
-cd /opt/
-git clone https://github.com/SecureAuthCorp/impacket.git | tee -a /etc/DOAZLAB/install-logs/060-impacket.log
-cd impacket
-python3.10 -m venv imp-env | tee -a /etc/DOAZLAB/install-logs/060-impacket.log
-source imp-env/bin/activate
-python3.10 -m pip install wheel | tee -a /etc/DOAZLAB/install-logs/060-impacket.log
-python3.10 -m pip install -r requirements.txt | tee -a /etc/DOAZLAB/install-logs/060-impacket.log
-python3.10 -m pip install . | tee -a /etc/DOAZLAB/install-logs/060-impacket.log
-deactivate
-cd /opt/
-
-
-# Next up: CrackMapExec
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-echo "Time: $(date). -- 70 --  CME" >> /etc/DOAZLAB/DOAZLABLog
-cd /opt/
-git clone https://github.com/DefensiveOrigins/APT22Things.git | tee -a /etc/DOAZLAB/install-logs/070-CME.log
-mv APT22Things CrackMap
-cd CrackMap
-python3.10 -m venv cme-venv
-source cme-venv/bin/activate
-python3.10 -m pip install wheel | tee -a /etc/DOAZLAB/install-logs/070-CME.log
-python3.10 -m pip install -r requirements.txt | tee -a /etc/DOAZLAB/install-logs/070-CME.log
-python3.10 cme | tee -a /etc/DOAZLAB/install-logs/070-CME.log
-deactivate
-cd /opt/
-
-
-# PlumHound
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-echo "Time: $(date). -- 80 --  PlumHound" >> /etc/DOAZLAB/DOAZLABLog
-cd /opt/ 
-git clone https://github.com/PlumHound/PlumHound.git | tee -a /etc/DOAZLAB/install-logs/080-PH.log
-cd PlumHound
-python3.10 -m venv ph-venv | tee -a /etc/DOAZLAB/install-logs/080-PH.log
-source ph-venv/bin/activate | tee -a /etc/DOAZLAB/install-logs/080-PH.log
-python3.10 -m pip install wheel | tee -a /etc/DOAZLAB/install-logs/080-PH.log
-python3.10 -m pip install -r requirements.txt | tee -a /etc/DOAZLAB/install-logs/080-PH.log
-python3.10 PlumHound.py | tee -a /etc/DOAZLAB/install-logs/080-PH.log
-deactivate
-cd /opt/
-
-
-# BloodHound.py
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-echo "Time: $(date). -- 90 --  BloodHound.py" >> /etc/DOAZLAB/DOAZLABLog
-cd /opt/
-git clone https://github.com/fox-it/BloodHound.py.git | tee -a /etc/DOAZLAB/install-logs/090-BH.log
-cd BloodHound.py
-python3.10 -m venv bh-env | tee -a /etc/DOAZLAB/install-logs/090-BH.log
-source bh-env/bin/activate
-python3.10 -m pip install wheel | tee -a /etc/DOAZLAB/install-logs/090-BH.log
-python3.10 setup.py install | tee -a /etc/DOAZLAB/install-logs/090-BH.log
-deactivate
-cd /opt/
-
-
-# BruteLoops
-# clone it, cd to it, add a venv container, activate, add wheel, install tools, deactivate
-echo "Time: $(date). -- 100 --  BruteLoops" >> /etc/DOAZLAB/DOAZLABLog
-
-cd /opt/
-git clone https://github.com/DefensiveOrigins/BruteLoops.git | tee -a /etc/DOAZLAB/install-logs/100-BruteLoops.log
-cd BruteLoops
-python3.10 -m venv bl-env | tee -a /etc/DOAZLAB/install-logs/100-BruteLoops.log
-source bl-env/bin/activate
-python3.10 -m pip install wheel | tee -a /etc/DOAZLAB/install-logs/100-BruteLoops.log
-python3.10 -m pip install -r requirements.txt | tee -a /etc/DOAZLAB/install-logs/100-BruteLoops.log
-deactivate
-cd /opt/
-
-
-# bl-bfg install (replace BruteLoops)
-# couple additional steps here, but seemed ok in testing
-echo "Time: $(date). -- 110 --  bl-bfg" >> /etc/DOAZLAB/DOAZLABLog
-cd /opt/
-git clone https://github.com/DefensiveOrigins/bl-bfg.git | tee -a /etc/DOAZLAB/install-logs/110-bl-bfg.log
-cd bl-bfg
-python3.10 -m venv bfg-env | tee -a /etc/DOAZLAB/install-logs/110-bl-bfg.log
-source bfg-env/bin/activate
-python3.10 -m pip install wheel | tee -a /etc/DOAZLAB/install-logs/110-bl-bfg.log
-python3.10 -m pip install . | tee -a /etc/DOAZLAB/install-logs/110-bl-bfg.log
-python3.10 setup.py install | tee -a /etc/DOAZLAB/install-logs/110-bl-bfg.log
-python3.10 -m pip install IPython | tee -a /etc/DOAZLAB/install-logs/110-bl-bfg.log
-deactivate
-cd /opt/
-
-
-# neo4j install
-echo "Time: $(date). -- 120 --  neo4j" >> /etc/DOAZLAB/DOAZLABLog
-echo "deb http://httpredir.debian.org/debian stretch-backports main" | sudo tee -a /etc/apt/sources.list.d/stretch-backports.list
-wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
-echo 'deb https://debian.neo4j.com stable 4.0' > /etc/apt/sources.list.d/neo4j.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9 648ACFD622F3D138 | tee -a /etc/DOAZLAB/install-logs/120-neo4j.log
-apt update | tee -a /etc/DOAZLAB/install-logs/120-neo4j.log
-apt install apt-transport-https -y | tee -a /etc/DOAZLAB/install-logs/120-neo4j.log
-apt install neo4j -y | tee -a /etc/DOAZLAB/install-logs/120-neo4j.log
-systemctl stop neo4j | tee -a /etc/DOAZLAB/install-logs/120-neo4j.log
-cd /usr/bin
-echo "dbms.default_listen_address=0.0.0.0" >> /etc/neo4j/neo4j.conf
+install_pipx
 
 
-# don't open the console dave. especially not during bootstrap
-systemctl start neo4j | tee -a /etc/DOAZLAB/install-logs/120-neo4j.log
+# Install CrackMapExec latest
+install_cme() {
+    # Find the path of crackmapexec
+    CME_PATH="/usr/bin/crackmapexec"
+    if [ -f "$CME_PATH" ]; then
+        sudo mv "$CME_PATH" "${CME_PATH}.old"
+        echo -e "[+] old crackmapexec installation from APT has been moved to ${CME_PATH}.old"
+        echo -e "[+] installing latest version of CrackMapExec via pipx"
+        [[ -d /opt/CrackMapExec ]] && cd /opt/CrackMapExec && pipx install .
+        # pipx ensurepath adds $HOME/.local/bin to ~/.zshrc
+        pipx ensurepath
+    else
+        echo -e "[+] Old crackmapexec version from APT is not installed : ) \n Installing latest version of CME"
+        [[ -d /opt/CrackMapExec ]] && cd /opt/CrackMapExec && pipx install .
+        # pipx ensurepath adds $HOME/.local/bin to ~/.zshrc
+        pipx ensurepath
+    fi
 
+    if [[ ":$PATH:" != *":${HOME}/.local/bin:"* ]]; then
+        export PATH="${PATH}:${HOME}/.local/bin"
+    fi
+    # Initialize CME database
+    "$HOME"/.local/bin/crackmapexec smb --help
+}
 
-# metasploit. 
-echo "Time: $(date). -- 130 --  metasploit" >> /etc/DOAZLAB/DOAZLABLog
-apt install -y build-essential zlib1g zlib1g-dev libpq-dev libpcap-dev libsqlite3-dev ruby ruby-dev | tee -a /etc/DOAZLAB/install-logs/130-MSF.log
-mkdir /opt/apps /opt/apps/msf
-cd /opt/apps/msf
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
-chmod 755 msfinstall
-./msfinstall | tee -a /etc/DOAZLAB/install-logs/130-MSF.log
+if [ -e "$HOME"/.local/bin/crackmapexec ]; then
+    echo -e "[+] CrackMapExec is already installed at ${HOME}/.local/bin/crackmapexec"
+else
+    install_cme
+fi
 
+# Install EyeWitness
+# EyeWitness project was not initially created with python3 virtual environments in mind and handles dependency installs via setup.sh
+# Long story short, if you try to use a virtualenv for EyeWitness, it will just be ignored unless you modify setup.sh to support them because the setup.sh bash script is not virtualenv context aware.
+# TL:DR no need for virtualenv for EyeWitness.
 
-# silenttrinity
-echo "Time: $(date). -- 140 --  silenttrinity" >> /etc/DOAZLAB/DOAZLABLog
-cd /opt/
-git clone https://github.com/DefensiveOrigins/SILENTTRINITY.git | tee -a /etc/DOAZLAB/install-logs/140-ST.log
-cd SILENTTRINITY/
-python3.10 -m venv st-env | tee -a /etc/DOAZLAB/install-logs/140-ST.log
-source st-env/bin/activate | tee -a /etc/DOAZLAB/install-logs/140-ST.log
-python3.10 -m pip install wheel | tee -a /etc/DOAZLAB/install-logs/140-ST.log
-python3.10 -m pip install -r requirements.txt | tee -a /etc/DOAZLAB/install-logs/140-ST.log
-deactivate
+echo -e "Installing EyeWitness"
+[[ -d /opt/EyeWitness/Python/setup ]] && cd /opt/EyeWitness/Python/setup && bash setup.sh
+# EyeWitness Selenium Fix
+python3 -m pip install selenium==4.9.1 --break-system-packages
+cd - &>/dev/null || exit 1
 
-
-# john the password ripper
-echo "Time: $(date). -- 150 --  john" >> /etc/DOAZLAB/DOAZLABLog
-mkdir -p ~/src
-apt -y install git build-essential libssl-dev zlib1g-dev | tee -a /etc/DOAZLAB/install-logs/150-John.log
-apt -y install yasm pkg-config libgmp-dev libpcap-dev libbz2-dev  | tee -a /etc/DOAZLAB/install-logs/150-John.log
-cd ~/src
-git clone https://github.com/openwall/john -b bleeding-jumbo john  | tee -a /etc/DOAZLAB/install-logs/150-John.log
-cd ~/src/john/src
-./configure && make -s clean && make -sj4  | tee -a /etc/DOAZLAB/install-logs/150-John.log
-
-
-# snag the hashes artifact archive
-echo "Time: $(date). -- 160 --  hashes" >> /etc/DOAZLAB/DOAZLABLog
-wget https://github.com/DefensiveOrigins/DO-LAB/raw/main/C2/artifacts/hashes.zip -O /opt/hashes.zip  | tee -a /etc/DOAZLAB/install-logs/160-hashes.log
-cd /opt/
-unzip hashes.zip  | tee -a /etc/DOAZLAB/install-logs/160-hashes.log
-rm hashes.zip
-
-
-echo "Time: $(date). Finished Installing Tools" >> /etc/DOAZLAB/DOAZLABLog
+# Install Evil-WinRM
+gem install evil-winrm
