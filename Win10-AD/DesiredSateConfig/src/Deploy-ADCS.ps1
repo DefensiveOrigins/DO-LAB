22159c74fff0854bfc9e221b83fbbb95bd1eb5c4
Relkci <kickler@blackhillsinfosec.com>
2024-01-15 23:31:30 -0700

    ADCS init1

---
+++ Win10-AD/DesiredSateConfig/src/Deploy-ADCS.ps1
@@ -0,0 +1,93 @@
+# Author: Roberto Rodriguez @Cyb3rWard0g
+# License: GPLv3
+
+configuration Deploy-ADCS {
+    param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$DomainFQDN,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds,
+
+    ) 
+    Import-DscResource -ModuleName ActiveDirectoryDsc, NetworkingDsc, xPSDesiredStateConfiguration, ComputerManagementDsc
+
+    [String] $DomainNetbiosName = (Get-NetBIOSName -DomainFQDN $DomainFQDN)
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    $Interface = Get-NetAdapter | Where-Object Name -Like "Ethernet*" | Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
+    $ComputerName = Get-Content env:computername
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {           
+            ConfigurationMode   = 'ApplyOnly'
+            RebootNodeIfNeeded  = $true
+        }
+
+        # ***** Install ADCS *****
+        xScript InstallADCS
+        {
+            SetScript = {
+                Get-WindowsFeature -Name AD-Certificate | Install-WindowsFeature
+                Add-WindowsFeature Adcs-Cert-Authority -IncludeManagementTools
+                Install-AdcsCertificationAuthority -CAType EnterpriseRootCA -Force
+
+                Add-WindowsFeature ADCS-Enroll-Web-Pol -IncludeManagementTools 
+                Add-WindowsFeature Adcs-Enroll-Web-Svc -IncludeManagementTools 
+                Add-WindowsFeature ADCS-Web-Enrollment -IncludeManagementTools 
+                Add-WindowsFeature ADCS-Device-Enrollment -IncludeManagementTools 
+                Add-WindowsFeature ADCS-Online-Cert -IncludeManagementTools 
+                
+                Install-AdcsEnrollmentPolicyWebService -Force
+                Install-AdcsEnrollmentWebService -Force
+                #Install-AdcsNetworkDeviceEnrollmentService -Force
+                Install-AdcsOnlineresponder -Force
+                Install-AdcsWebEnrollment -Force
+
+
+                #Add Default templates
+                Add-CATemplate "ClientAuth" -Force
+                Add-CATemplate "CodeSigning" -Force 
+                Add-CATemplate "Workstation" -Force
+                Add-CATemplate "SmartcardUser" -Force
+                Add-CATemplate "ExchangeUser" -Force
+                Add-CATemplate "EnrollmentAgent" -Force
+
+                #Install module to manage import/export of templates
+                Install-Module ADCSTemplate -Force
+                #Export-ADCSTemplate vuln_Template > vuln_template.json
+
+                #Download DOLAB templates 
+                $wc = new-object System.Net.WebClient
+                $wc.DownloadFile('https://raw.githubusercontent.com/DefensiveOrigins/DO-LAB/main/Win10-AD/resources/ca_templates/vuln_template1.json', 'C:\ProgramData\vuln_template1.json')
+                $wc.DownloadFile('https://raw.githubusercontent.com/DefensiveOrigins/DO-LAB/main/Win10-AD/resources/ca_templates/vuln_template4.json', 'C:\ProgramData\vuln_template4.json')
+
+                #Import DOLAB templates
+                New-ADCSTemplate -DisplayName Vuln_Template1 -JSON (Get-Content C:\ProgramData\vuln_template1.json -Raw) -Publish
+                New-ADCSTemplate -DisplayName Vuln_Template4 -JSON (Get-Content C:\ProgramData\vuln_template4.json -Raw) -Publish
+
+            }
+            GetScript =  
+            {
+                # This block must return a hashtable. The hashtable must only contain one key Result and the value must be of type String.
+                return @{ "Result" = "false" }
+            }
+            TestScript = 
+            {
+                # If it returns $false, the SetScript block will run. If it returns $true, the SetScript block will not run.
+                return $false
+            }
+        }
+
+        PendingReboot RebootOnSignalFromAADConnect
+        {
+            Name        = 'RebootOnSignalFromADCS'
+            DependsOn   = "[xScript]InstallADCS"
+        }
+
+    }
+}
