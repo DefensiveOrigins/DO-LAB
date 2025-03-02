f0fd71bb3b1a75a47b606da19d7f587d73391fe1
Relkci <29710634+Relkci@users.noreply.github.com.>
2025-03-02 04:01:46 -0700

    unecessary IE FRU

--- Deploy-AD/scripts/Set-EdgeFRU.ps1
+++ Deploy-AD/scripts/Set-EdgeFRU.ps1
@@ -10,5 +10,3 @@ if (!(Test-Path $edgeRegPath)) {
 Set-ItemProperty -Path $edgeRegPath -Name "HideFirstRunExperience" -Type DWord -Value 1
 Set-ItemProperty -Path $edgeRegPath -Name "PreventFirstRunPage" -Type DWord -Value 1
 
-# Disable IE ESC for administrators
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A90BCB0D-5B68-4950-8FC2-48E0A74F54E7}" -Name "IsInstalled" -Value 0
