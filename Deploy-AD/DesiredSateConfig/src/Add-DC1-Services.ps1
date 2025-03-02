5d7e256e339f7e382b1a987b91cab4c7e513c0b9
Relkci <29710634+Relkci@users.noreply.github.com.>
2025-03-02 01:41:24 -0700

    try to fix race condition with dc promo breakout

---
+++ Deploy-AD/DesiredSateConfig/src/Add-DC1-Services.ps1
@@ -0,0 +1,121 @@
+# Author: Roberto Rodriguez @Cyb3rWard0g
+# License: GPLv3
+# References:
+# https://github.com/Azure/azure-quickstart-templates/blob/master/sharepoint-adfs/dsc/ConfigureDCVM.ps1
+configuration ADD-DC1-Services {
+    param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$DomainFQDN,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds,
+
+        [Parameter(Mandatory)]
+        [Object]$DomainUsers
+    ) 
+    
+    Import-DscResource -ModuleName ActiveDirectoryDsc, NetworkingDsc, xPSDesiredStateConfiguration, xDnsServer, ComputerManagementDsc
+    
+    [String] $DomainNetbiosName = (Get-NetBIOSName -DomainFQDN $DomainFQDN)
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    $Interface = Get-NetAdapter | Where-Object Name -Like "Ethernet*" | Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
+    $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AdminCreds.Password)
+    $AdminPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+    $ComputerName = Get-Content env:computername
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {           
+            ConfigurationMode   = 'ApplyOnly'
+            RebootNodeIfNeeded  = $true
+        }
+
+        # ***** Add DNS and AD Features *****
+        WindowsFeature DNS 
+        { 
+            Ensure  = "Present" 
+            Name    = "DNS"		
+        }
+
+        Script EnableDNSDiags
+        {
+            SetScript = { 
+                Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
+            }
+            GetScript   = { @{} }
+            TestScript  = { $false }
+            DependsOn   = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature DnsTools
+        {
+            Ensure      = "Present"
+            Name        = "RSAT-DNS-Server"
+            DependsOn   = "[WindowsFeature]DNS"
+        }
+
+        DnsServerAddress SetDNS 
+        { 
+            Address         = '127.0.0.1' 
+            InterfaceAlias  = $InterfaceAlias
+            AddressFamily   = 'IPv4'
+            DependsOn       = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure      = "Present" 
+            Name        = "AD-Domain-Services"
+            DependsOn   = "[WindowsFeature]DNS" 
+        } 
+
+        WindowsFeature ADDSTools
+        {
+            Ensure      = "Present"
+            Name        = "RSAT-ADDS-Tools"
+            DependsOn   = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure      = "Present"
+            Name        = "RSAT-AD-AdminCenter"
+            DependsOn   = "[WindowsFeature]ADDSTools"
+        }
+        
+
+        PendingReboot RebootOnSignalFromADAdminCenter
+        {
+            Name        = 'RebootOnSignalFromADAdminCenter'
+            DependsOn   = "[ADDomain]ADAdminCenter"
+        }
+    }
+}
+
+function Get-NetBIOSName {
+    [OutputType([string])]
+    param(
+        [string]$DomainFQDN
+    )
+
+    if ($DomainFQDN.Contains('.')) {
+        $length = $DomainFQDN.IndexOf('.')
+        if ( $length -ge 16) {
+            $length = 15
+        }
+        return $DomainFQDN.Substring(0, $length)
+    }
+    else {
+        if ($DomainFQDN.Length -gt 15) {
+            return $DomainFQDN.Substring(0, 15)
+        }
+        else {
+            return $DomainFQDN
+        }
+    }
+}
---
+++ Deploy-AD/DesiredSateConfig/src/Add-DC2-Domain.ps1
@@ -0,0 +1,77 @@
+# Author: Roberto Rodriguez @Cyb3rWard0g
+# License: GPLv3
+# References:
+# https://github.com/Azure/azure-quickstart-templates/blob/master/sharepoint-adfs/dsc/ConfigureDCVM.ps1
+configuration Add-DC2-Domain {
+    param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$DomainFQDN,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds,
+
+        [Parameter(Mandatory)]
+        [Object]$DomainUsers
+    ) 
+    
+    Import-DscResource -ModuleName ActiveDirectoryDsc, NetworkingDsc, xPSDesiredStateConfiguration, xDnsServer, ComputerManagementDsc
+    
+    [String] $DomainNetbiosName = (Get-NetBIOSName -DomainFQDN $DomainFQDN)
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    $Interface = Get-NetAdapter | Where-Object Name -Like "Ethernet*" | Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
+    $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AdminCreds.Password)
+    $AdminPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+    $ComputerName = Get-Content env:computername
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {           
+            ConfigurationMode   = 'ApplyOnly'
+            RebootNodeIfNeeded  = $true
+        }
+         
+        # ****** Create AD Domain *********
+        ADDomain CreateADForest 
+        {
+            DomainName                      = $DomainFQDN
+            Credential                      = $DomainCreds
+            SafemodeAdministratorPassword   = $DomainCreds
+            DatabasePath                    = "C:\NTDS"
+            LogPath                         = "C:\NTDS"
+            SysvolPath                      = "C:\SYSVOL"
+        }
+
+        PendingReboot RebootOnSignalFromCreateADForest
+        {
+            Name        = 'RebootOnSignalFromCreateADForest'
+            DependsOn   = "[ADDomain]CreateADForest"
+        }
+    }
+}
+
+function Get-NetBIOSName {
+    [OutputType([string])]
+    param(
+        [string]$DomainFQDN
+    )
+
+    if ($DomainFQDN.Contains('.')) {
+        $length = $DomainFQDN.IndexOf('.')
+        if ( $length -ge 16) {
+            $length = 15
+        }
+        return $DomainFQDN.Substring(0, $length)
+    }
+    else {
+        if ($DomainFQDN.Length -gt 15) {
+            return $DomainFQDN.Substring(0, 15)
+        }
+        else {
+            return $DomainFQDN
+        }
+    }
+}
