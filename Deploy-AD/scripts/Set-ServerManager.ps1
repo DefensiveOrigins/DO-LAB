870b8eb3226c0af1852e04999c16b5fd81abdec3
Relkci <29710634+Relkci@users.noreply.github.com.>
2025-03-02 03:19:02 -0700

    update SM FRU

--- Deploy-AD/scripts/Set-ServerManager.ps1
+++ Deploy-AD/scripts/Set-ServerManager.ps1
@@ -1,4 +1,3 @@
-# Author: Roberto Rodriguez (@Cyb3rWard0g)
-# License: GPL-3.0
+
 
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\ServerManager" -Name "DoNotOpenServerManagerAtLogon" -Value 1 -PropertyType DWORD -Force
