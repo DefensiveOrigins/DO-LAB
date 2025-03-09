03b9cfa2c6f4c92c53b6054d3f0321abab0c669e
Kent Ickler <29710634+Relkci@users.noreply.github.com>
2025-03-09 01:06:56 -0700

    Update Set-Initial-Settings.ps1

--- Deploy-AD/scripts/Set-Initial-Settings.ps1
+++ Deploy-AD/scripts/Set-Initial-Settings.ps1
@@ -72,3 +72,10 @@ else
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" /v "EnableFeeds" /t REG_DWORD /d 0 /f
     Stop-Process -Name explorer -Force
 }
+
+# Add the CheckContext.ps1 Script
+New-Item -ItemType Directory -Path "C:\DOAZLab\" -Force > $null
+New-Item -ItemType Directory -Path "C:\DOAZLab\Scripts\" -Force > $null
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DefensiveOrigins/DO-LAB/refs/heads/main/Deploy-AD/resources/CheckContext.ps1" -OutFile "C:\doazlab\Scripts\CheckContext.ps1"
+
+
