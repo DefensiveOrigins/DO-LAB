948168a38a1f425f67e76e56c8a001712ca20b1c
Kent Ickler <29710634+Relkci@users.noreply.github.com>
2025-03-09 01:11:28 -0700

    Update CheckContext.ps1

--- Deploy-AD/resources/CheckContext.ps1
+++ Deploy-AD/resources/CheckContext.ps1
@@ -10,13 +10,13 @@ if (-not $HostName) {
 if ($env:COMPUTERNAME -ne $HostName) {
     Write-Host "`n`nWARN: Wrong system. Expected: $HostName, Found: $env:COMPUTERNAME. Exiting...`n`n"
     pause
-    exit
+    Stop-Process -Id $PID
 }
 
 if ($env:USERNAME -ne "DOADMIN" -or $env:USERDOMAIN -ne "DOAZLAB") {
     Write-Host "`n`nWARN: Wrong user. Expected: DOAZLAB\DOADMIN, Found: $env:USERDOMAIN\$env:USERNAME. Exiting...`n`n"
     pause
-    exit
+    Stop-Process -Id $PID
 }
 
 Write-Host "`n`nINFO: Running on correct system: $env:COMPUTERNAME as $env:USERDOMAIN\$env:USERNAME`n`n"
