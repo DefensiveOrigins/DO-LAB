70c6409ea9ffbc3506ece2ee43fc1171a1636cb4
Kent Ickler <29710634+Relkci@users.noreply.github.com>
2025-03-09 01:35:00 -0700

    Update CheckContext.ps1

--- Deploy-AD/resources/CheckContext.ps1
+++ Deploy-AD/resources/CheckContext.ps1
@@ -2,6 +2,17 @@ param (
     [string]$HostName
 )
 
+# Check if running as Administrator
+$adminCheck = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$isAdmin = (New-Object System.Security.Principal.WindowsPrincipal $adminCheck).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $isAdmin) {
+    Write-Host "`n`nWARN: This script must be run as an Administrator. Exiting...`n`n"
+    pause
+    Stop-Process -Id $PID
+}
+
+
 if (-not $HostName) {
     Write-Host "No hostname provided. Exiting..."
     exit
@@ -19,4 +30,4 @@ if ($env:USERNAME -ne "DOADMIN" -or $env:USERDOMAIN -ne "DOAZLAB") {
     Stop-Process -Id $PID
 }
 
-Write-Host "`n`nINFO: Running on correct system: $env:COMPUTERNAME as $env:USERDOMAIN\$env:USERNAME`n`n"
+Write-Host "`n`nINFO: Running on correct system: $env:COMPUTERNAME as $env:USERDOMAIN\$env:USERNAME with Administrator privileges.`n`n"
